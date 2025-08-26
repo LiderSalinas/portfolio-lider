c9acb40d8bd6fb82356a651cbffbdc4bf1420961
Lider Salinas <rubensalinas05@gmail.com>
2025-08-26 11:45:09 -0300

    fix(footer): corrige enlace de LinkedIn y markup JSX

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,18 @@ export default function Footer() {
   return (
     <footer className="text-center py-3 mt-5 border-top">
       <small>
-        © 2025 Líder Salinas ·{" "}
-        <a href="mailto:rubensalinas05@gmail.com">rubensalinas05@gmail.com</a> ·
-        <a
-          href="https://www.linkedin.com/in/l%C3%ADder-rub%C3%A9n-salinas-blanco-729705250
-          target="_blank"
-          rel="noreferrer"
-          className="ms-2"
-        >
-          LinkedIn
-        </a>
-      </small>
+  © 2025 Líder Salinas ·{" "}
+  <a
+    href="https://www.linkedin.com/in/l%C3%ADder-rub%C3%A9n-salinas-blanco-729705250"
+    target="_blank"
+    rel="noreferrer"
+    className="ms-2"
+  >
+    LinkedIn
+  </a>{" "}
+  ·{" "}
+  <a href="mailto:rubensalinas05@gmail.com">rubensalinas05@gmail.com</a>
+</small>
     </footer>
   );
 }
