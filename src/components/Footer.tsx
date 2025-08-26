ff770d3ffae12f281b283abfe09b5d9b6dda5df5
Lider Salinas <rubensalinas05@gmail.com>
2025-08-26 11:58:15 -0300

    fix: LinkedIn absoluto (https) como <a> externo en Footer/Hero

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,14 @@ export default function Footer() {
       <small>
   © 2025 Líder Salinas ·{" "}
   <a
-    href="https://www.linkedin.com/in/l%C3%ADder-rub%C3%A9n-salinas-blanco-729705250"
-    target="_blank"
-    rel="noreferrer"
-    className="ms-2"
-  >
-    LinkedIn
-  </a>{" "}
+  href="https://www.linkedin.com/in/l%C3%ADder-rub%C3%A9n-salinas-blanco-729705250"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="ms-2"
+>
+  LinkedIn
+</a>
+{" "}
   ·{" "}
   <a href="mailto:rubensalinas05@gmail.com">rubensalinas05@gmail.com</a>
 </small>
